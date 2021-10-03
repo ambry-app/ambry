0a902aae6f837ba97f9104d6285944d5d7ca2de7
Chris Dosé <chris.dose@gmail.com>
2021-10-02 23:36:31 -0700

    add more statuses to media for better visibility
    closes #28

--- lib/ambry/media/processor.ex
+++ lib/ambry/media/processor.ex
@@ -5,7 +5,9 @@ defmodule Ambry.Media.Processor do
   Delegates to other modules depending on what kind of files were uploaded.
   """
 
-  use Oban.Worker, queue: :media
+  use Oban.Worker,
+    queue: :media,
+    max_attempts: 1
 
   alias Ambry.Media
   alias Ambry.Media.Processor.{MP3, MP3Concat}
@@ -14,6 +16,19 @@ defmodule Ambry.Media.Processor do
   def perform(%Oban.Job{args: %{"media_id" => id}}) do
     media = Media.get_media!(id)
 
+    {:ok, media} = Media.update_media(media, %{status: :processing})
+
+    try do
+      run_processor!(media)
+    rescue
+      exception ->
+        {:ok, _media} = Media.update_media(media, %{status: :error})
+
+        reraise exception, __STACKTRACE__
+    end
+  end
+
+  defp run_processor!(media) do
     cond do
       MP3Concat.can_run?(media) -> MP3Concat.run(media)
       MP3.can_run?(media) -> MP3.run(media)
