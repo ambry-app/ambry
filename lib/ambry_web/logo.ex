cf1f3652c3f4b056289f7227a49cc12a92b4922a
Chris Dosé <chris.dose@gmail.com>
2021-10-04 17:58:41 -0700

    rework logo to look better at smaller sizes

--- lib/ambry_web/logo.ex
+++ lib/ambry_web/logo.ex
@@ -10,7 +10,7 @@ defmodule AmbryWeb.Logo do
   def logo_with_tagline(assigns) do
     ~H"""
     <h1 class="text-center">
-      <img class="mx-auto h-20" alt="Ambry" src={Routes.static_path(@conn, "/images/logo_optimized.svg")}>
+      <img class="mx-auto" style="max-height: 128px;" alt="Ambry" src={Routes.static_path(@conn, "/images/logo_256x1056.svg")}>
       <span class="font-semibold text-gray-500">Personal Audiobook Streaming</span>
     </h1>
     """
