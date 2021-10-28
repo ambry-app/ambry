813267bdd755718e92c3efae2bc1a0faeb404bdb
Chris Dosé <chris.dose@gmail.com>
2021-10-27 17:58:01 -0700

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ambry.MixProject do
   def project do
     [
       app: :ambry,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
