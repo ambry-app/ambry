e3805875dfb3f6cb6a9a7d5fa0df6000d1d524e2
Chris Dosé <chris.dose@gmail.com>
2021-10-31 15:56:49 -0700

    Hotfix: don't assume all player state fields
    * API should accept partial params

--- lib/ambry_web/controllers/api/player_state_controller.ex
+++ lib/ambry_web/controllers/api/player_state_controller.ex
@@ -26,11 +26,14 @@ defmodule AmbryWeb.API.PlayerStateController do
   def update(conn, %{"id" => media_id, "playerState" => params}) do
     player_state = Media.get_or_create_player_state!(conn.assigns.api_user.id, media_id)
 
-    {:ok, player_state} =
-      Media.update_player_state(player_state, %{
-        position: params["position"],
-        playback_rate: params["playbackRate"]
-      })
+    attrs =
+      Enum.reduce(params, %{}, fn
+        {"position", position}, acc -> Map.put(acc, :position, position)
+        {"playbackRate", rate}, acc -> Map.put(acc, :playback_rate, rate)
+        _ignore, acc -> acc
+      end)
+
+    {:ok, player_state} = Media.update_player_state(player_state, attrs)
 
     render(conn, "show.json", player_state: player_state)
   end
