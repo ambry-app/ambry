84a3d937a040889419008c736495050f7788467b
Chris Dosé <chris.dose@gmail.com>
2021-11-26 22:50:25 -0800

    Add MP4 re-encode processor and ability to choose
    * Allows forced re-encoding of bad MP4s
    * Allows you to choose which processor you want to run

--- lib/ambry/media/processor.ex
+++ lib/ambry/media/processor.ex
@@ -8,21 +8,22 @@ defmodule Ambry.Media.Processor do
   import Ambry.Media.Processor.Shared, only: [out_path: 1]
 
   alias Ambry.Media
-  alias Ambry.Media.Processor.{MP3, MP3Concat, MP4, MP4Concat}
+  alias Ambry.Media.Processor.{MP3, MP3Concat, MP4, MP4Concat, MP4ReEncode}
 
   @processors [
     MP3,
     MP3Concat,
     MP4,
-    MP4Concat
+    MP4Concat,
+    MP4ReEncode
   ]
 
-  def run!(media) do
+  def run!(media, processor \\ :auto) do
     {:ok, media} = Media.update_media(media, %{status: :processing}, for: :processor_update)
 
     try do
-      ensure_out_path!(media)
-      run_processor!(media)
+      ensure_clean_out_path!(media)
+      run_processor!(media, processor)
     rescue
       exception ->
         {:ok, _media} = Media.update_media(media, %{status: :error}, for: :processor_update)
@@ -31,21 +32,26 @@ defmodule Ambry.Media.Processor do
     end
   end
 
-  defp ensure_out_path!(media) do
-    media |> out_path() |> File.mkdir_p!()
+  defp ensure_clean_out_path!(media) do
+    path = out_path(media)
+    File.rm_rf!(path)
+    File.mkdir_p!(path)
   end
 
-  defp run_processor!(media) do
-    case matched_processors(media) do
-      [processor] -> processor.run(media)
-      [] -> raise "No matching processor found!"
-      [_ | _] -> raise "Multiple matching processors found!"
+  defp run_processor!(media, processor) do
+    if processor == :auto do
+      case matched_processors(media) do
+        [processor | _] -> processor.run(media)
+        [] -> raise "No matching processor found!"
+      end
+    else
+      processor.run(media)
     end
   end
 
-  defp matched_processors(media) do
+  def matched_processors(media_or_filenames) do
     Enum.filter(@processors, fn processor ->
-      processor.can_run?(media)
+      processor.can_run?(media_or_filenames)
     end)
   end
 end
--- lib/ambry/media/processor/mp4.ex
+++ lib/ambry/media/processor/mp4.ex
@@ -5,10 +5,20 @@ defmodule Ambry.Media.Processor.MP4 do
 
   import Ambry.Media.Processor.Shared
 
+  alias Ambry.Media.Media
+
   @extensions ~w(.mp4 .m4a .m4b)
 
-  def can_run?(media) do
+  def name do
+    "Single MP4 As-is"
+  end
+
+  def can_run?(%Media{} = media) do
     media |> files(@extensions) |> length() == 1
+  end
+
+  def can_run?(filenames) when is_list(filenames) do
+    filenames |> filter_filenames(@extensions) |> length() == 1
   end
 
   def run(media) do
