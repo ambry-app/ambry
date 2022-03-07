bd1d3969a3798e92300c5e51bf1a76c1270fa18c
Chris Dosé <chris.dose@gmail.com>
2022-03-06 22:41:41 -0800

    Fix up library styling

--- lib/ambry_web/live/now_playing_live/index.ex
+++ lib/ambry_web/live/now_playing_live/index.ex
@@ -6,4 +6,18 @@ defmodule AmbryWeb.NowPlayingLive.Index do
   use AmbryWeb, :p_live_view
 
   import AmbryWeb.NowPlayingLive.Index.Components
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    assigns =
+      case socket.assigns do
+        %{player_state: player_state} ->
+          [page_title: player_state.media.book.title]
+
+        _else ->
+          [page_title: "Personal Audiobook Streaming"]
+      end
+
+    {:ok, assign(socket, assigns)}
+  end
 end
