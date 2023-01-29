09bbd0a893c8501029371979234cadba43314b20
Chris Dosé <221693+doughsay@users.noreply.github.com>
2023-01-29 14:06:53 -0800

    Smooth time bar seeking

--- new/assets/js/hooks/timeBar.js
+++ new/assets/js/hooks/timeBar.js
@@ -1,9 +1,13 @@
 export const TimeBarHook = {
   mounted () {
     this.dragging = false
-    this.width = this.el.firstElementChild.clientWidth
+    this.wrapper = this.el
+    this.timeBar = this.wrapper.firstElementChild
+    this.progressBar = this.timeBar.firstElementChild
+    this.handle = this.timeBar.lastElementChild
+    this.width = this.timeBar.clientWidth
 
-    this.attach(this.el, "mousedown")
+    this.attach(this.wrapper, "mousedown")
     this.attach(window, "mousemove")
     this.attach(window, "mouseup")
     this.attach(window, "resize")
@@ -17,9 +21,14 @@ export const TimeBarHook = {
     this.listeners.push([target, event, callback])
   },
 
+  updateUI () {
+    this.progressBar.style.width = `${this.percent}%`
+    this.handle.style.left = `calc(${this.percent}% - 8px)`
+  },
+
   mousedown (event) {
     if (event.buttons === 1) {
-      this.dragging = true
+      this.startDragging()
     }
 
     event.preventDefault()
@@ -31,17 +40,35 @@ export const TimeBarHook = {
     this.position = Math.min(x, this.width)
     this.ratio = this.position / this.width
     this.percent = (this.ratio * 100).toFixed(2)
+
+    if (this.isDragging()) {
+      this.updateUI()
+    }
   },
 
   mouseup (event) {
-    if (this.dragging) {
-      this.dragging = false
+    if (this.isDragging()) {
+      this.endDragging()
       mediaPlayer.seekRatio(this.ratio)
     }
   },
 
   resize (event) {
-    this.width = this.el.firstElementChild.clientWidth
+    this.width = this.timeBar.clientWidth
+  },
+
+  startDragging () {
+    this.dragging = true
+    this.wrapper.setAttribute('phx-update', 'ignore')
+  },
+
+  endDragging () {
+    this.dragging = false
+    this.wrapper.removeAttribute('phx-update')
+  },
+
+  isDragging () {
+    return this.dragging
   },
 
   destroyed () {
