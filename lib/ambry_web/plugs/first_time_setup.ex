1929cec3695f69436bffc03aea232da1d6c90b15
Chris Dosé <chris@xn--dos-dma.com>
2023-03-07 00:38:20 -0800

    Restore first-time-setup fix

--- lib/ambry_web/plugs/first_time_setup.ex
+++ lib/ambry_web/plugs/first_time_setup.ex
@@ -11,20 +11,21 @@ defmodule AmbryWeb.Plugs.FirstTimeSetup do
   alias Plug.Conn
 
   @impl Plug
-  @spec init([]) :: boolean()
-  def init([]) do
-    Application.get_env(:ambry, :first_time_setup, false)
-  end
+  @spec init([]) :: []
+  def init([]), do: []
 
   @impl Plug
-  @spec call(Conn.t(), boolean) :: Conn.t()
-  def call(conn, false), do: conn
+  @spec call(Conn.t(), []) :: Conn.t()
+  def call(conn, []) do
+    first_time_setup? = Application.get_env(:ambry, :first_time_setup, false)
+    setup_path? = match?(%Conn{path_info: ["first_time_setup" | _]}, conn)
 
-  def call(%Conn{path_info: ["first_time_setup" | _]} = conn, true), do: conn
-
-  def call(conn, true) do
-    conn
-    |> redirect(to: ~p"/first_time_setup")
-    |> Conn.halt()
+    if first_time_setup? and not setup_path? do
+      conn
+      |> redirect(to: ~p"/first_time_setup")
+      |> Conn.halt()
+    else
+      conn
+    end
   end
 end
