7d7e40f52844a40fae1509dfdc399a9fdb627a1e
Chris Dosé <chris@xn--dos-dma.com>
2023-03-07 23:45:40 -0800

    Update esbuild and tailwind

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :ambry, Ambry.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.41",
+  version: "0.17.11",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.4",
+  version: "3.2.7",
   default: [
     args: ~w(
       --config=tailwind.config.js
