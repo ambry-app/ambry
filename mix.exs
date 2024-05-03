7747be453dfd3bde37304f5ccc843f2a0826ad1b
Chris Dosé <chris@xn--dos-dma.com>
2024-05-03 14:41:58 -0700

    Shaka packager and player dependency updates

--- mix.exs
+++ mix.exs
@@ -100,7 +100,7 @@ defmodule Ambry.MixProject do
     [
       {:"decimal.js", "10.4.3"},
       {:"platform-detect", "3.0.1"},
-      {:"shaka-player", "4.7.11"},
+      {:"shaka-player", "4.8.1"},
       {:topbar, "2.0.2"}
     ]
   end
