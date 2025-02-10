7d851b1817264db3012351afa6e46f72e1c87bb4
Chris Dosé <chris@xn--dos-dma.com>
2025-02-09 17:54:51 -0800

    Fix seed files downloader for split files

--- script/download_seed_files.sh
+++ script/download_seed_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-SEED_FILES=https://github.com/ambry-app/ambry/releases/download/v1.4.0/uploads.tar.gz
+curl -Lo uploads00.tar.gz https://github.com/ambry-app/ambry/releases/download/v1.4.0/uploads00.tar.gz
+curl -Lo uploads01.tar.gz https://github.com/ambry-app/ambry/releases/download/v1.4.0/uploads01.tar.gz
+curl -Lo uploads02.tar.gz https://github.com/ambry-app/ambry/releases/download/v1.4.0/uploads02.tar.gz
 
-curl -Lo uploads.tar.gz $SEED_FILES
-
-tar -xvzf uploads.tar.gz
+cat uploads*.tar.gz | tar xzpvf -
+tar -xvf uploads.tar.gz
 
 rm uploads.tar.gz
+rm uploads00.tar.gz
+rm uploads01.tar.gz
+rm uploads02.tar.gz
