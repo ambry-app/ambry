9eaa559de1e2324eb0ab0693d0fa56f957510cf7
Chris Dosé <chris@xn--dos-dma.com>
2025-09-07 11:40:25 -0700

    Update ecto

--- lib/ambry/repo.ex
+++ lib/ambry/repo.ex
@@ -13,44 +13,4 @@ defmodule Ambry.Repo do
       result -> {:ok, result}
     end
   end
-
-  @doc """
-  Runs the given function inside a transaction.
-
-  This function is a wrapper around `Ecto.Repo.transaction`, with the following differences:
-
-  - It accepts only a lambda of arity 0 or 1 (i.e. it doesn't work with multi).
-  - If the lambda returns `:ok | {:ok, result}` the transaction is committed.
-  - If the lambda returns `:error | {:error, reason}` the transaction is rolled back.
-  - If the lambda returns any other kind of result, an exception is raised, and the transaction is rolled back.
-  - The result of `transact` is the value returned by the lambda.
-
-  This function accepts the same options as `Ecto.Repo.transaction/2`.
-  """
-  @spec transact((-> result) | (module -> result), Keyword.t()) :: result
-        when result: :ok | {:ok, any} | :error | {:error, any}
-  def transact(fun, opts \\ []) do
-    transaction_result =
-      transaction(
-        fn repo ->
-          lambda_result =
-            case Function.info(fun, :arity) do
-              {:arity, 0} -> fun.()
-              {:arity, 1} -> fun.(repo)
-            end
-
-          case lambda_result do
-            :ok -> {__MODULE__, :transact, :ok}
-            :error -> rollback({__MODULE__, :transact, :error})
-            {:ok, result} -> result
-            {:error, reason} -> rollback(reason)
-          end
-        end,
-        opts
-      )
-
-    with {outcome, {__MODULE__, :transact, outcome}}
-         when outcome in [:ok, :error] <- transaction_result,
-         do: outcome
-  end
 end
