54dfc69c1d28a7a819b5f467aaaaa481d37d2dd5
Chris Dosé <chris.dose@gmail.com>
2025-12-27 20:40:05 -0800

    Deep link to app on preview and audiobook pages (#1093)

--- lib/ambry_web/controllers/preview/audiobook_controller.ex
+++ lib/ambry_web/controllers/preview/audiobook_controller.ex
@@ -1,25 +1,37 @@
 defmodule AmbryWeb.Preview.AudiobookController do
   use AmbryWeb, :controller
 
+  import Absinthe.Relay.Node, only: [to_global_id: 3]
+  import AmbryWeb.Helpers.IdHelpers
+
   alias Ambry.Media
 
-  def show(conn, %{"id" => media_id}) do
-    media = Media.get_media_with_book_details!(media_id)
+  def show(conn, %{"id" => id_param}) do
+    with {:ok, media_id} <- parse_id(id_param, :media),
+         {:ok, media} <- Media.fetch_media_with_book_details(media_id) do
+      description = (media |> Media.get_media_description() |> String.slice(0..100)) <> "..."
+      global_id = to_global_id("Media", media.id, AmbrySchema)
 
-    description = (media |> Media.get_media_description() |> String.slice(0..100)) <> "..."
-
-    conn
-    |> put_session(:user_return_to, current_path(conn))
-    |> render(:show, %{
-      media: media,
-      page_title: description,
-      og: %{
-        title: description,
-        image: media.thumbnails && unverified_url(conn, media.thumbnails.extra_large),
-        description: media.description && truncate_markdown(media.description),
-        url: url(conn, ~p"/audiobooks/#{media.id}")
-      }
-    })
+      conn
+      |> put_session(:user_return_to, current_path(conn))
+      |> render(:show, %{
+        media: media,
+        global_id: global_id,
+        page_title: description,
+        og: %{
+          title: description,
+          image: media.thumbnails && unverified_url(conn, media.thumbnails.extra_large),
+          description: media.description && truncate_markdown(media.description),
+          url: url(conn, ~p"/audiobooks/#{media.id}")
+        }
+      })
+    else
+      _ ->
+        conn
+        |> put_status(:not_found)
+        |> put_view(AmbryWeb.ErrorHTML)
+        |> render(:"404")
+    end
   end
 
   defp truncate_markdown(markdown) do
