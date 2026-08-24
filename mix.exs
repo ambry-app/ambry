f6b10aa08e200d0826019f4e4b68646a97313ff5
Chris Dosé <chris@xn--dos-dma.com>
2026-08-23 18:02:35 -0700

    Bump to 2.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ambry.MixProject do
   def project do
     [
       app: :ambry,
-      version: "2.6.0",
+      version: "2.7.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:boundary, :phoenix_live_view] ++ Mix.compilers(),
